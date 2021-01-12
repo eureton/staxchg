94b62581d22a630d5a2ff0e27248b3e36477253f
eureton <agis@mailbox.org>
2021-01-12 23:06:58 +0200

    cap log token length

--- src/staxchg/dev.clj
+++ src/staxchg/dev.clj
@@ -3,8 +3,19 @@
 
 (def pathname "./dev.log")
 
+(defn truncate
+  [x]
+  (if-let [_ (string? x)]
+    (let [length (count x)
+          truncation-limit 128
+          context 32]
+      (if (> length truncation-limit)
+        (str (subs x 0 context) " [...] " (subs x (- length context) length))
+        x))
+    x))
+
 (defn log
   [& items]
   (with-open [writer (clojure.java.io/writer pathname :append true)]
-    (.write writer (str (apply str items) "\n"))))
+    (.write writer (str (apply str (map truncate items)) "\n"))))
 
