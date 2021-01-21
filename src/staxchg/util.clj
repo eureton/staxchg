1399f35a2e262abab99a8d666b713139e9bfa8c4
eureton <agis@mailbox.org>
2021-01-21 12:19:13 +0200

    ns for helper functions

---
+++ src/staxchg/util.clj
@@ -0,0 +1,24 @@
+(ns staxchg.util
+  (:gen-class))
+
+(defn read-properties
+  ""
+  [pathname]
+  (let [properties (java.util.Properties.)]
+    (with-open [stream (clojure.java.io/reader pathname)]
+      (.load properties stream))
+    properties))
+
+(defn properties-hash
+  ""
+  [pathname]
+  (reduce
+    (fn [m [k v]] (assoc m k v))
+    {}
+    (read-properties pathname)))
+
+(defn config-pathname
+  ""
+  []
+  (str (System/getenv "HOME") "/.staxchg.conf"))
+
