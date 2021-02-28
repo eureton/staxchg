ff28cd45cc78f26cbd1f110b41a932175bd9658d
eureton <agis@mailbox.org>
2021-02-28 17:16:23 +0200

    update clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/eureton/staxchg"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/core.async "1.3.610"]
                  [clj-http "3.10.3"]
                  [cheshire "5.10.0"]
